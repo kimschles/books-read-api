55bf5b285921ed748ddb18e2f69d5158788d5fc5
coloradokim <schlesinger.kim@gmail.com>
2018-04-21 22:40:46 -0600

    update port and add video link

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
+const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get('/books', (req, res) => res.send('Hello World!'));
+app.get('/books', (req, res) => res.send('Hello Books!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, () => console.log('Example app is listening!'));
--- tutorial-outcomes/00-Project-Setup.md
+++ tutorial-outcomes/00-Project-Setup.md
@@ -1,4 +1,6 @@
-# Tutorial: Build a RESTful API
+# Tutorial: Build a RESTful API using Node, Express, Knex and PostgreSQL
+
+[Video: 00-Project-Setup](https://youtu.be/CTLHys7EgEA)
 
 ## Project Setup
 
@@ -10,4 +12,5 @@ By the end of this video, you will be able to:
   * Knex
   * pg
 * [ ] Spin up a simple server
+* [ ] Commit code and it push to github
 * [ ] Deploy to Heroku
