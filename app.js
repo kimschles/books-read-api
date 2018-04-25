2c2773502b09f49737f6229c9370b8334aa3d567
coloradokim <schlesinger.kim@gmail.com>
2018-04-24 19:53:01 -0600

    add routes and logging

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
+const bookDatabase = require('./connection');
 const app = express();
 
-app.get('/api/v1/books', (req, res) => res.send('Hello Books!'));
+app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'));
+
+app.get('/api/v1/books', (req, res) => {
+  bookDatabase('book')
+    .select('*')
+    .then(books => res.status(200).json(books))
+    .catch(error => {
+      res.status(500).json({ error: error.message, stack: error.stack });
+    });
+});
+
+app.get('/api/v1/books/:id', (req, res) => {
+  bookDatabase('book')
+    .select('*')
+    .limit(1)
+    .where({ id: req.params.id })
+    .then(book => res.status(200).json(book))
+    .catch(error => {
+      res.status(500).json({ error: error.message, stack: error.stack });
+    });
+});
 
 app.listen(PORT, () => console.log('Example app is listening!'));
---
+++ tutorial-outcomes/02-Build-Read-Routes.md
@@ -0,0 +1,14 @@
+# Tutorial: Build a RESTful API using Node, Express, Knex and PostgreSQL
+
+[Video: 02-Build-Read-Routes]()
+
+## Build Read Routes
+
+By the end of this video, you will be able to:
+
+* Create a file that connects your database to your server
+* Build a route that displays all the data from the book table
+* Add the Morgan package to show more information about `http` requests
+* Build a route that displays the data for one book
+* Commit and push code to github
+* Deploy to Heroku
